d7efe9f768253bbbd4a3c038ce751761e0771f3e
Jameyi-Zhou <13548127972@163.com>
2024-01-14 17:07:37 +0800

    .

--- VG-train/train.sh
+++ VG-train/train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # ReferItGame
 # python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 8 --lr_bert 0.00001 --aug_crop --aug_scale --aug_translate --backbone resnet50 --detr_model ./checkpoints/detr-r50-referit.pth --bert_enc_num 12 --detr_enc_num 6 --dataset referit --max_query_len 20 --output_dir outputs/referit_r50
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 # # RefCOCO
 # python -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --batch_size 32 --lr 5e-6 --lr_bert 5e-7 --lr_visu_tra 5e-7 --lr_visu_cnn 5e-7 --aug_crop --aug_scale --aug_translate --backbone resnet50 --detr_model ./checkpoints/detr-r50.pth --bert_enc_num 12 --detr_enc_num 6 --vl_enc_layers 6 --dataset unc --max_query_len 20 --output_dir outputs/refcoco_r50 --epoch 150 --resume outputs/refcoco_r50/checkpoint0119.pth
-python -m torch.distributed.launch --nproc_per_node=1 --use_env train.py --batch_size 4 --aug_crop --aug_scale --aug_translate --bert_enc_num 12 --vl_enc_layers 6 --dataset unc --max_query_len 20 --output_dir outputs/refcoco_vit_base20x20 --epoch 90 --imsize 320 --resume checkpoint.pth
+python -m torch.distributed.launch --nproc_per_node=1 --use_env train.py --batch_size 4 --aug_crop --aug_scale --aug_translate --bert_enc_num 12 --vl_enc_layers 6 --dataset unc --max_query_len 20 --output_dir outputs/refcoco --epoch 90 --imsize 320
 
 
 # # RefCOCO+
